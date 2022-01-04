78c86dbd8c40702b5b9d70ee7a7f381b5f760418
Mamertmg <marquezgarcia.a@gmail.com>
2022-01-04 20:41:51 +0100

    commit on Master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import HomeScreen from './screens/HomeScreen.js';
+import { Container} from 'react-bootstrap';
 
-function App() {
+const App = () => {
   return (
     <div>
-      <h1>Welcome To ProShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />      
+        </Container>
+      </main>
+      <Footer />      
     </div>
+
   );
 }
 
